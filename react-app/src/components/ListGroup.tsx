d597af0affed4edbb8c650788a1924689da91871
Ashley Hachey <ahachey17@gmail.com>
2023-12-19 20:49:02 -0500

    45 mins into React w/ Mosh

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,31 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  //Event handler, smirf
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  // no for loop, convert each item to an item of a different type
+  //below is them items in the array converted to the list items
   return (
-    <h1>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>Not item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          //needs key property to keep track of the items
+          //to know which part needs to be updated
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
-    </h1>
+    </>
   );
 }
 
